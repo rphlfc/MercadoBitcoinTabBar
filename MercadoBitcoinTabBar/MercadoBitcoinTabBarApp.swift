f0c860c2c68f173509fcd3ea13050880f449a213
Raphael Cerqueira <raphaelfc@gmail.com>
2021-11-26 18:42:09 -0300

    Initial Commit

---
+++ MercadoBitcoinTabBar/MercadoBitcoinTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MercadoBitcoinTabBarApp.swift
+//  MercadoBitcoinTabBar
+//
+//  Created by Raphael Cerqueira on 26/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MercadoBitcoinTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
